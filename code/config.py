da1be23634eeb610c3d11b01decf631a8b2daa58
gijswijnholds <gijswijnholds@gmail.com>
2021-01-19 18:33:51 +0100

    Add stress test paths

--- code/config.py
+++ code/config.py
@@ -9,18 +9,21 @@ mbert = "bert-base-multilingual-cased"
 data_folder = 'sick_nl/data'
 stress_test_folder = os.path.join(data_folder, 'tasks/stress_tests')
 sick_folder = os.path.join(data_folder, 'tasks/sick_nl')
+vectors_folder = os.path.join(data_folder, 'vectors')
 
-sick_path = os.path.join(sick_folder, 'SICK.txt')
-sicknl_path = os.path.join(sick_folder, 'SICK_NL.txt')
+sick_fn_en = os.path.join(sick_folder, 'SICK.txt')
+sick_fn_nl = os.path.join(sick_folder, 'SICK_NL.txt')
 
-skipgram_fn = os.path.join(data_folder, 'GoogleNews-vectors-negative300_SICK.txt')
-skipgram_fn_nl = os.path.join(data_folder, '320/wikipedia-320.txt')
-
-sick_fn = os.path.join(data_folder, 'SICK.txt')
-sick_fn_nl = os.path.join(data_folder, 'SICK_NL.txt')
+skipgram_fn = os.path.join(vectors_folder, 'GoogleNews-vectors-negative300_SICK.txt')
+skipgram_fn_nl = os.path.join(vectors_folder, '320/wikipedia-320.txt')
 
 models_folder = os.path.join(data_folder, 'models')
+models_agg_folder = os.path.join(data_folder, 'models_agg')
 results_folder = os.path.join(data_folder, 'results')
 
-prep_order_fn = os.path.join(data_folder, 'tasks/stress_tests/prep_phrase_order.txt')
-present_tense_fn =
+prep_order_fn = os.path.join(stress_test_folder, 'prep_phrase_order.txt')
+present_tense_fn = os.path.join(stress_test_folder, 'present_cont_present_simple.txt')
+
+best_bert_nl_model = os.path.join(models_agg_folder, 'model_SICK_NL_bert-base-dutch-cased_epoch16.pt')
+best_roberta_nl_model = os.path.join(models_agg_folder, 'model_SICK_NL_robbert-v2-dutch-base_epoch19.pt')
+best_mbert_nl_model = os.path.join(models_agg_folder, 'model_SICK_NL_bert-base-multilingual-cased_epoch16.pt')
