c93cd63904e7f61a3a0744f79fcd3087fcfceb38
gijswijnholds <gijswijnholds@gmail.com>
2021-01-19 18:34:14 +0100

    Update main.py

--- code/main.py
+++ code/main.py
@@ -3,12 +3,13 @@ from sick_nl.code.evaluation.eval_relatedness \
 from sick_nl.code.evaluation.eval_nli \
     import evaluate_en_nli_models, evaluate_nl_nli_models
 from sick_nl.code.evaluation.eval_stresstests \
-    import evaluate_on_stress_tests
+    import evaluate_switched_sicks, evaluate_stress_tests
 
 
 def main():
-    evaluate_en_models()
-    evaluate_nl_models()
-    evaluate_en_nli_models()
-    evaluate_nl_nli_models()
-    evaluate_on_stress_tests()
+    sick_en_relatedness_results = evaluate_en_models()
+    sick_nl_relatedness_results = evaluate_nl_models()
+    sick_en_nli_results = evaluate_en_nli_models()
+    sick_nl_nli_results = evaluate_nl_nli_models()
+    stress_test_results_insick = evaluate_switched_sicks()
+    stress_test_results = evaluate_stress_tests()
